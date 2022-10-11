70bb616bacac3fe33900229ee4900880a408eab6
kyleecodes <kfieldsinmd@gmail.com>
2022-10-10 20:46:26 -0400

    Update branding & misc. edits:
    - about me & index intro updated
    - favicon updated
    - instagram link added to socials
    - adjusted color theme to fix brown hue
    - subtitle and home link added in header
    - link to devrel resources added in about

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -37,10 +37,11 @@ function SocialLink() {
       </div>
 
       <div className={styles.left_link_container}>
-        <a href="https://communaltech.notion.site/Kylee-Fields-Communal-Tech-0730bd5aa6a24f6d9e91b895e56cee48">
-          <h2> learn more! </h2>{" "}
+        <a href="https://www.instagram.com/kylee.codes/">
+          <h2> instagram </h2>{" "}
         </a>
       </div>
+
     </div>
   );
 }
--- layouts/Header.js
+++ layouts/Header.js
@@ -1,18 +1,12 @@
 import styles from "../styles/Header.module.css";
-import Navbar from "../components/NavBar";
-
-import dynamic from "next/dynamic";
-
-const ThemeToggle = dynamic(() => import("../components/ThemeToggle"), {
-  ssr: false,
-});
 
 function Header() {
   return (
     <div className={styles.header_container}>
-      <div className={styles.company_title}>Communal Tech</div>
-      {/* Commenting out to save for later  */}
-      {/* <div className={styles.subtitle}> technical community management</div> */}
+      <a href="/">
+        <div className={styles.company_title}>Communal Tech</div>
+        <div className={styles.subtitle}> DevRel + Community + Content</div>
+      </a>
     </div>
   );
 }
