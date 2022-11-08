5ab77fdeb3323f0ea4b31f721781329ccb56db7d
Kylee Fields <kfieldsinmd@gmail.com>
2022-11-07 21:00:14 -0500

    Added Calendly link to socials

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -6,6 +6,16 @@ function SocialLink() {
       <div className={styles.right_link_container}>
         <a
           target="_blank"
+          href="https://calendly.com/communaltech"
+          rel="noopener noreferrer"
+        >
+          <h2> calendly </h2>
+        </a>
+      </div>
+
+      <div className={styles.left_link_container}>
+        <a
+          target="_blank"
           href="https://blljhazo9cp.typeform.com/to/vOzyqBjF"
           rel="noopener noreferrer"
         >
@@ -13,30 +23,30 @@ function SocialLink() {
         </a>
       </div>
 
-      <div className={styles.left_link_container}>
+      <div className={styles.right_link_container}>
         <a href="https://communaltech.hashnode.dev/">
           <h2> blog </h2>{" "}
         </a>
       </div>
 
-      <div className={styles.right_link_container}>
+      <div className={styles.left_link_container}>
         <a href="https://twitter.com/communal_tech">
           <h2> twitter </h2>{" "}
         </a>
       </div>
 
-      <div className={styles.left_link_container}>
+      <div className={styles.right_link_container}>
         <a href="https://github.com/kyleecodes">
           <h2> github </h2>{" "}
         </a>
       </div>
-      <div className={styles.right_link_container}>
+      <div className={styles.left_link_container}>
         <a href="https://www.linkedin.com/in/kyleecodes/">
           <h2> linkedIn </h2>
         </a>
       </div>
 
-      <div className={styles.left_link_container}>
+      <div className={styles.right_link_container}>
         <a href="https://www.instagram.com/kylee.codes/">
           <h2> instagram </h2>{" "}
         </a>
